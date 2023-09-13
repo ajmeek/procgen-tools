b73c17a94e8c1dd88dd7de5bf0b3ade42ea57ac1
Austin <austin.meek10@gmail.com>
2023-09-12 20:38:09 -0400

    linear mapping. find right coordinates now

--- playground/paper_graphics/paper_graphics.py
+++ playground/paper_graphics/paper_graphics.py
@@ -17,6 +17,47 @@ seed = 0
 What's the role of axis size in matplotlib above? I thought axes were like layers in gimp.
 """
 
+# ------------------------------------------------ util functions------------------------------------------------
+
+def linear_mapping(coords):
+    """
+    This function takes in 16x16 coordinates and outputs 25x25 coordinates.
+    This assumes that the far corners (0,0) and (15,15) map to (0,0) and (24, 24).
+
+    Keep in mind that when displaying these new coordinates, care must be taken.
+    For most of the seeds we'll be using (aka 13x13 inner grid), we need to make sure that the coordinates lie
+    within that. So patching activations in the 16x16 grid that maps outside of this inner core is not a good idea.
+    """
+
+    # Define the linear transformation coefficients
+    A = 25 / 16  # Scale factor in x-direction
+    E = 25 / 16  # Scale factor in y-direction
+
+    # Apply the linear transformation
+    u = A * coords[0]
+    v = E * coords[1]
+
+
+    # Round the mapped coordinates to the nearest integers
+    u_rounded = np.round(u).astype(int)
+    v_rounded = np.round(v).astype(int)
+
+    if not 6 <= u_rounded <= 18:
+        print("u rounded is not in the inner grid")
+    if not 6 <= v_rounded <= 18:
+        print("v rounded is not in the inner grid")
+
+    return u_rounded, v_rounded
+
+
+# test the above
+print(linear_mapping((0,0)))
+print(linear_mapping((15,15)))
+print(linear_mapping((8,8)))
+print(linear_mapping((5,10)))
+
+# ---------------------------------------------------- fig 1 ----------------------------------------------------
+
 cheese_channel = 55
 
 def fig_1():
@@ -130,6 +171,9 @@ def fig_2():
     axd2['cheese_a'].imshow(activ)
     axd2['cheese_a'].imshow(activ, cmap='RdBu', vmin=-1, vmax=1)
 
+    axd2['cheese_a'].set_xticks([])
+    axd2['cheese_a'].set_yticks([])
+
     maze.move_cheese_in_state(state, cheese_b_pos)
     venv.env.callmethod('set_state', [state.state_bytes])
     obs = t.tensor(venv.reset(), dtype=t.float32)
@@ -141,6 +185,9 @@ def fig_2():
     activ = hook.get_value_by_label(default_layer)[0][cheese_channel]
     axd2['cheese_b'].imshow(activ)
     axd2['cheese_b'].imshow(activ, cmap='RdBu', vmin=-1, vmax=1)
+
+    axd2['cheese_b'].set_xticks([])
+    axd2['cheese_b'].set_yticks([])
 
     maze.move_cheese_in_state(state, cheese_c_pos)
     venv.env.callmethod('set_state', [state.state_bytes])
@@ -154,11 +201,14 @@ def fig_2():
     axd2['cheese_c'].imshow(activ)
     axd2['cheese_c'].imshow(activ, cmap='RdBu', vmin=-1, vmax=1)
 
+    axd2['cheese_c'].set_xticks([])
+    axd2['cheese_c'].set_yticks([])
+
 
     #plt.show()
     plt.savefig('playground/paper_graphics/visualizations/fig_2.svg', bbox_inches="tight", format='svg')
 
-fig_2()
+#fig_2()
 
 # ---------------------------------------------------- fig 3 ----------------------------------------------------
 
@@ -167,10 +217,15 @@ fig_2()
 
 # ---------------------------------------------------- fig 4 ----------------------------------------------------
 def fig_4():
-    success_a_pos = (5,6)
-    success_b_pos = (12,12)
-    success_c_pos = (0,0) #(8,9) - too close to original mpp location
-    fail = (0,12) #(2,3) - fail, but so is everything
+    #patching works on a 16x16 grid. Putting the dot near there on a 25x25 grid requires some massaging.
+
+    # Mrinank wants this done with a linear mapping from 16x16 to 25x25.
+
+    #These are done with the 16x16 grid. Check the resulting 25x25 coordinates afterwards too.
+    success_a_pos = (0, 15)
+    success_b_pos = (15, 15)
+    success_c_pos = (3, 3) #(8,9) - too close to original mpp location
+    fail = (4, 12) #(2,3) - fail, but so is everything
 
     fig4, axd4 = plt.subplot_mosaic(
         [['success_a', 'success_b', 'success_c', 'fail']],
@@ -188,8 +243,9 @@ def fig_4():
 
     #viz.visualize_venv(venv, show_plot=True, render_padding=False)
 
+    u, v = linear_mapping(success_a_pos)
     padding = maze.get_padding(maze.get_inner_grid_from_seed(seed))
-    viz.plot_pixel_dot(axd4['success_a'], success_a_pos[0], success_a_pos[1], hidden_padding=padding)
+    viz.plot_pixel_dot(axd4['success_a'], u, v, hidden_padding=padding)
     #viz.plot_red_dot(venv, axd4['success_a'], success_a_pos[0], success_a_pos[1])
     axd4['success_a'].imshow(img)
 
@@ -198,8 +254,9 @@ def fig_4():
         patched_vfield = viz.vector_field(venv, hook.network)
     img = viz.plot_vf_mpp(patched_vfield, ax=axd4['success_b'], save_img=False)
 
+    u, v = linear_mapping(success_b_pos)
     padding = maze.get_padding(maze.get_inner_grid_from_seed(seed))
-    viz.plot_pixel_dot(axd4['success_b'], success_b_pos[0], success_b_pos[1], hidden_padding=padding)
+    viz.plot_pixel_dot(axd4['success_b'], u, v, hidden_padding=padding)
     axd4['success_b'].imshow(img)
 
     patches = patch_utils.get_channel_pixel_patch(layer_name=default_layer,channel=55, value=5, coord=success_c_pos)
@@ -207,8 +264,9 @@ def fig_4():
         patched_vfield = viz.vector_field(venv, hook.network)
     img = viz.plot_vf_mpp(patched_vfield, ax=axd4['success_c'], save_img=False)
 
+    u, v = linear_mapping(success_c_pos)
     padding = maze.get_padding(maze.get_inner_grid_from_seed(seed))
-    viz.plot_pixel_dot(axd4['success_c'], success_c_pos[0], success_c_pos[1], hidden_padding=padding)
+    viz.plot_pixel_dot(axd4['success_c'], u, v, hidden_padding=padding)
     axd4['success_c'].imshow(img)
 
     patches = patch_utils.get_channel_pixel_patch(layer_name=default_layer,channel=55, value=5, coord=fail)
@@ -216,10 +274,11 @@ def fig_4():
         patched_vfield = viz.vector_field(venv, hook.network)
     img = viz.plot_vf_mpp(patched_vfield, ax=axd4['fail'], save_img=False)
 
+    u, v = linear_mapping(fail)
     padding = maze.get_padding(maze.get_inner_grid_from_seed(seed))
-    viz.plot_pixel_dot(axd4['fail'], fail[0], fail[1], hidden_padding=padding)
+    viz.plot_pixel_dot(axd4['fail'], u, v, hidden_padding=padding)
     axd4['fail'].imshow(img)
 
     #plt.show()
     plt.savefig('playground/paper_graphics/visualizations/fig_4.svg', bbox_inches="tight", format='svg')
-#fig_4()
+fig_4()
